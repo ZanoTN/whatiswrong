627f0d79d8215de40aee1b4ea5279c4d1a89db63
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2025-12-27 21:11:18 +0000

    Fix custom initializers

--- config/initializers/after_app_start.rb
+++ config/initializers/after_app_start.rb
@@ -1,5 +1,6 @@
 Rails.application.config.after_initialize do
-  next unless Rails.server?
+  next if defined?(Rails::Console)
+  next unless Rails.const_defined?(:Server)
 
   AfterAppStart.run
 end
