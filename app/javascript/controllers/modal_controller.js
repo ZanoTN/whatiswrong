9be3ff29daaea61d198a4805fda373834651973a
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2026-01-29 18:38:59 +0000

    Refactor confirm modals

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,19 +2,46 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal"
 export default class extends Controller {
+  static values = { type: String };
+
   connect() {
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
+    // Listener per l'escape key
+    this.escapeListener = (event) => {
+      if (event.key === "Escape" && this.canBeClosed()) {
         this.close();
       }
-    });
+    };
+    document.addEventListener("keydown", this.escapeListener);
+  }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.escapeListener);
   }
 
   close() {
-    // Delete the content of the turbo_frame_tag "modal" to close the modal
-    const modalFrame = document.getElementById("modal");
+    const modalFrame = document.getElementById(this.getModalIdValue());
     if (modalFrame) {
       modalFrame.innerHTML = "";
     }
+  }
+
+  canBeClosed() {
+    if (this.getModalIdValue() === "confirm-modal") {
+      return false;
+    }
+
+    if (this.getModalIdValue() === "modal") {
+      const confirmModal = document.getElementById("confirm_modal");
+      if (confirmModal && confirmModal.innerHTML.trim() !== "") {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // Restituisce l'ID del modal o "modal" di default
+  getModalIdValue() {
+    return this.hasTypeValue ? this.typeValue : "modal";
   }
 }
